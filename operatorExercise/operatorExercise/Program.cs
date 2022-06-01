b7be759da3deea620ffc21d1bec63646226e0b34
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-01 10:28:02 -0400

    operator exercise part 1

---
+++ operatorExercise/operatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace operatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 5;
+
+            int quotient = a /= b;
+            int remainder = a %= b;
+
+            Console.WriteLine($"18/5 is {quotient} remainder {remainder}");
+
+            
+        }
+    }
+}
+
