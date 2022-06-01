41c84be5eaf3e0d9ab0a542fbea5caf26d6eda12
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-01 11:27:48 -0400

    update integers

--- operatorExercise/operatorExercise/Program.cs
+++ operatorExercise/operatorExercise/Program.cs
@@ -6,13 +6,13 @@ namespace operatorExercise
     {
         static void Main(string[] args)
         {
-            int a = 18;
-            int b = 5;
+            int a = 17;
+            int b = 4;
 
             int quotient = a /= b;
             int remainder = a %= b;
 
-            Console.WriteLine($"18/5 is {quotient} remainder {remainder}");
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
             Console.WriteLine("What is the radius of your circle?");
             var radius = double.Parse(Console.ReadLine());
             double circleArea = Math.PI * Math.Pow(radius, 2);
